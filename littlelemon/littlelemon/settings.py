e4c1fe9b5e50c4a52bd3460c8cbe25efaadbbe64
Sayyid Faruk Romdoni <126539714+sayyidfaruk@users.noreply.github.com>
2026-04-14 12:22:56 +0000

    add auth and secure the table booking API

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "restaurant",
     "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,14 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+DJOSER = {
+    'USER_ID_FIELD': 'username',
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 from restaurant import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
@@ -27,4 +28,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
 ]
